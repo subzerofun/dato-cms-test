266d9a7464b2e3a3e5bf9a8722ebc2243fb416d5
subzerofun <dietrichstein@gmx.net>
2017-06-18 02:18:59 +0200

    initial commit

---
+++ content/post/my-post.md
@@ -0,0 +1,10 @@
+---
+title: First article
+type: post
+categories:
+  - random
+weight: 4
+date: '2012-04-06'
+---
+
+Lorem **ipsum dolor sit amet**, consectetur adipiscing elit.
--- dato.config.js
+++ dato.config.js
@@ -1,5 +1,14 @@
 // dato.config.js
 
 module.exports = (dato, root, i18n) => {
-
+  root.createPost("content/post/my-post.md", "yaml", {
+    frontmatter: {
+      title: "First article",
+      type: "post",
+      categories: ["random"],
+      weight: 4,
+      date: "2012-04-06",
+    },
+    content: "Lorem **ipsum dolor sit amet**, consectetur adipiscing elit."
+  });
 }
